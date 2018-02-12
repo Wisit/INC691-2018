c6201115f2f904ec56e902011400c4cd2a73811a
DrSanti <santi.inc.kmutt@gmail.com>
2018-02-12 22:22:13 +0700

    add ex01_main

---
+++ edf/library/header/atcmd.h
@@ -0,0 +1,6 @@
+#ifndef __AT_CMD_H__
+#define __AT_CMD_H__
+
+#include "config.h"
+
+#endif
---
+++ edf/user/ex01_main/main.c
@@ -0,0 +1,16 @@
+#include "os.h"
+
+/* Global variables */
+
+/* Other Functions  */
+
+int main(void) {
+    
+	/* Initialization */
+	
+	OS_Init();
+
+	/* Other OS's functions */
+    
+    OS_Start();
+}
