9a4b1885c6e5ad15c649b5a362f4c4ee95a011d9
DrSanti <santi.inc.kmutt@gmail.com>
2018-02-12 23:13:55 +0700

    add ex02_led

---
+++ Embedded-Development-Framework/user/ex02_led/main.c
@@ -0,0 +1,34 @@
+#include "os.h"
+
+int main(void) {
+
+    uint16_t ticks = 0;
+
+	OS_Init();
+
+    LED_Set(LED_ID_3);                  // Set LED3
+    while(1) {
+        
+        if( OS_TickedCheck() ) {        // OS tick is activated/ticked every 1 mS
+
+            ticks++;                    // Increase ticks by 1
+
+            if (ticks > 500) {
+                ticks = 0;
+                LED_Inv(LED_ID_0);      // Toggle LED0
+                LED_Toggle(LED_ID_3);   // Toggle LED3
+            }
+            else if (ticks % 250 == 0)  {
+                LED_Inv(LED_ID_2);      // Toggle LED2
+            }
+            else if (ticks == 50) {
+                LED_Set(LED_ID_1);      // Set LED1
+            }
+            else if (ticks == 90) {
+                LED_Clr(LED_ID_1);      // Clear LED1
+            }
+        }
+    }
+
+    OS_Start();
+}
