82e902b7c1417ff50aa17d75509b670e6dd0ba9e
drsanti <santi.inc.kmutt@gmail.com>
2018-02-13 12:52:56 +0700

    Update main.c

--- Embedded-Development-Framework/user/ex01_main/main.c
+++ Embedded-Development-Framework/user/ex01_main/main.c
@@ -1,16 +1,20 @@
 #include "os.h"
 
 /* Global variables */
+//...
 
 /* Other Functions  */
+//...
 
 int main(void) {
     
     /* Variables and hardware initialization */
+    //...
 	
     OS_Init();	// Initialise the OS
 
     /* Other OS's functions */
-    
+    //...
+	
     OS_Start();	// Start the OS
 }
